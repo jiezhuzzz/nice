a0679097d7b20908a7184d772ae0226035b4dbc9
jiezhuzzz <jiezzz@duck.com>
2026-04-23 09:06:35 -0500

    update

--- modules/home-manager/common/claude-code.nix
+++ modules/home-manager/common/claude-code.nix
@@ -5,6 +5,12 @@
     rev = "cf62a6c02dc03db88da8eb7c61bdb9fd88da6326";
     sha256 = "d28cc99927aa4b2d09ee077d3043e2ecfcc6d09971677b53b6f1f2816b72889b";
   };
+  codex-plugin-cc = pkgs.fetchFromGitHub {
+    owner = "openai";
+    repo = "codex-plugin-cc";
+    rev = "v1.0.4";
+    sha256 = "cd675dcf5f1cdc4d794cfb84be3324064af088594add9a881b960fe715fa6482";
+  };
 in {
   programs.claude-code = {
     enable = true;
@@ -27,18 +33,14 @@ in {
         repo = "superpowers";
         rev = "v5.0.7";
         sha256 = "1d0b4ef5c65f3cf2241c38fae0d790b86f69f568522815645865a1664663668a";
+        name = "superpowers";
       })
       "${claude-plugins-official}/plugins/skill-creator"
       "${claude-plugins-official}/plugins/code-review"
       "${claude-plugins-official}/plugins/code-simplifier"
       "${claude-plugins-official}/plugins/agent-sdk-dev"
       "${claude-plugins-official}/plugins/ralph-loop"
-      (pkgs.fetchFromGitHub {
-        owner = "openai";
-        repo = "codex-plugin-cc";
-        rev = "v1.0.4";
-        sha256 = "cd675dcf5f1cdc4d794cfb84be3324064af088594add9a881b960fe715fa6482";
-      })
+      "${codex-plugin-cc}/plugins/codex"
     ];
   };
 }
