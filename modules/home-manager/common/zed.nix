0edee20cccef79a82347a3001ad54fb3f77a2521
jiezhuzzz <jiezzz@duck.com>
2026-05-05 00:56:30 -0500

    feat(zed): configure LSPs, formatters, and editor preferences
    Wire up nil + alejandra (Nix), texlab + tex-fmt (LaTeX), and
    ruff + ty (Python, replacing default pyright). Enable helix mode,
    bracket colorization, soft wrap, and switch to the no-ligature
    JetBrainsMonoNL Nerd Font.

--- modules/home-manager/common/zed.nix
+++ modules/home-manager/common/zed.nix
@@ -1,11 +1,49 @@
 {pkgs, ...}: {
-  programs.zed-editor.enable = true;
-  programs.zed-editor.package =
-    if pkgs.stdenv.isDarwin
-    then null
-    else pkgs.zed-editor;
-  programs.zed-editor.userSettings = {
-    buffer_font_family = "JetBrainsMono Nerd Font";
-    buffer_font_size = 14;
+  programs.zed-editor = {
+    enable = true;
+    extensions = ["nix" "latex"];
+    extraPackages = with pkgs; [
+      nil
+      alejandra
+      texlab
+      tex-fmt
+      ruff
+      ty
+    ];
+    userSettings = {
+      buffer_font_family = "JetBrainsMonoNL Nerd Font";
+      buffer_font_size = 14;
+      helix_mode = true;
+      colorize_brackets = true;
+      soft_wrap = "editor_width";
+      languages = {
+        Nix = {
+          language_servers = ["nil"];
+          formatter = {
+            external = {
+              command = "alejandra";
+              arguments = ["-q" "-"];
+            };
+          };
+          format_on_save = "on";
+        };
+        LaTeX = {
+          language_servers = ["texlab"];
+          tab_size = 2;
+          formatter = {
+            external = {
+              command = "tex-fmt";
+              arguments = ["--stdin" "--nowrap" "--format-tables"];
+            };
+          };
+          format_on_save = "on";
+        };
+        Python = {
+          language_servers = ["ruff" "ty" "!pyright"];
+          formatter = "language_server";
+          format_on_save = "on";
+        };
+      };
+    };
   };
 }
