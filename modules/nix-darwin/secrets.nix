781eb8ed5f0fea6fea119caa9726f3bbe69fbd85
jiezhuzzz <jiezzz@duck.com>
2026-07-06 19:34:33 -0500

    chore(modules): flatten default.nix-only leaf modules to plain files
    Convention: leaf modules are single .nix files; directories only for
    composites (nixos/desktop, nixos/media, ...) or modules with assets
    (ghostty/). Moves fonts, homebrew, secrets, system (nix-darwin) and boot,
    secrets (nixos) up one level; relative paths inside adjusted.

--- modules/nix-darwin/secrets.nix
+++ modules/nix-darwin/secrets.nix
@@ -1,48 +1,48 @@
 {inputs, ...}: let
-  user = import ../../../users/jie.nix;
+  user = import ../../users/jie.nix;
 in {
   imports = [inputs.agenix.darwinModules.default];
 
   age.identityPaths = ["/etc/ssh/ssh_host_ed25519_key"];
 
   age.secrets.github-ssh-key = {
-    file = ../../../secrets/ssh/github.age;
+    file = ../../secrets/ssh/github.age;
     owner = user.me.username;
     mode = "0400";
   };
 
   age.secrets.git-signing-key = {
-    file = ../../../secrets/ssh/git-signing.age;
+    file = ../../secrets/ssh/git-signing.age;
     owner = user.me.username;
     mode = "0400";
   };
 
   age.secrets.chameleon-ssh-key = {
-    file = ../../../secrets/ssh/chameleon.age;
+    file = ../../secrets/ssh/chameleon.age;
     owner = user.me.username;
     mode = "0400";
   };
 
   age.secrets.lab-ssh-key = {
-    file = ../../../secrets/ssh/lab.age;
+    file = ../../secrets/ssh/lab.age;
     owner = user.me.username;
     mode = "0400";
   };
 
   age.secrets.home-ssh-key = {
-    file = ../../../secrets/ssh/home.age;
+    file = ../../secrets/ssh/home.age;
     owner = user.me.username;
     mode = "0400";
   };
 
   age.secrets.rclone-gdrive-token = {
-    file = ../../../secrets/rclone/gdrive.age;
+    file = ../../secrets/rclone/gdrive.age;
     owner = user.me.username;
     mode = "0400";
   };
 
   age.secrets.rclone-box-token = {
-    file = ../../../secrets/rclone/box.age;
+    file = ../../secrets/rclone/box.age;
     owner = user.me.username;
     mode = "0400";
   };
